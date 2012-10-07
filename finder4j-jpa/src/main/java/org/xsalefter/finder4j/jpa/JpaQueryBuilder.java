1c0f276f9e4c49baea278961bb9d7f9b342db62e
xsalefter <xsalefter@gmail.com>
2012-10-07 12:49:39 +0700

    - Align with superclass. - Change algorithm about how to parse Restriction.

--- finder4j-jpa/src/main/java/org/xsalefter/finder4j/jpa/JpaQueryBuilder.java
+++ finder4j-jpa/src/main/java/org/xsalefter/finder4j/jpa/JpaQueryBuilder.java
@@ -13,7 +13,6 @@ import org.xsalefter.finder4j.Restriction;
 import org.xsalefter.finder4j.RestrictionType;
 import org.xsalefter.finder4j.spi.AbstractQueryBuilder;
 import org.xsalefter.finder4j.spi.RestrictionHandler;
-import org.xsalefter.finder4j.spi.RestrictionHandler.DTO;
 
 /**
  * Implementation of {@link QueryBuilder} for Java Persistence API.
@@ -27,7 +26,7 @@ public class JpaQueryBuilder extends AbstractQueryBuilder {
 
 	public JpaQueryBuilder(final Class<?> entityClass) {
 		super();
-		this.createEntityAliasAndNameFromClass(entityClass);
+		this.createEntityAndAliasNameFromClass(entityClass);
 		this.addRestrictionHandlers(new JpaRestrictionHandlerFactory(this));
 
 		super.getCountQueryStringBuilder().append("select ").
@@ -82,13 +81,11 @@ public class JpaQueryBuilder extends AbstractQueryBuilder {
 
 		// Used to handle internal 'where' criteria/restriction.
 		final StringBuilder whereQueryString = new StringBuilder();
-
-		int logicCounter = 1;
 		final int restrictionSize = restrictions.size();
 
-		for (int i = 0; i < restrictionSize; i++) {
+		for (int i = 0; i < restrictions.size(); i++) {
 			final Restriction restriction = restrictions.get(i);
-			restriction.setParameter(super.getRestrictionSize() + 1); // Any better idea than this??
+			restriction.setParameter(super.getActualRestrictionSize() + 1); // Any better idea than this??
 
 			final RestrictionType restrictionType = restriction.getRestrictionType();
 			final RestrictionHandler handler = super.getRestrictionHandler(restrictionType);
@@ -98,36 +95,39 @@ public class JpaQueryBuilder extends AbstractQueryBuilder {
 			final boolean isRestrictionParameterized = dto.hasParameterizedQueryString();
 
 			if (isRestrictionParameterized)
-				super.addRestriction(restriction.getParameter(), restriction);
+				super.addActualRestriction(restriction.getParameter(), restriction);
 
-			if (logicCounter < restrictionSize) {
-				final Restriction nextRestriction = restrictions.get(logicCounter);
-				final DTO nextRestrictionHandlerDTO = handler.handleRestriction(nextRestriction);
-				final boolean isNextRestrictionNeedParam = nextRestrictionHandlerDTO.hasParameterizedQueryString();
+			final String stringLogic = restriction.getRestrictionLogic().toLowerCase();
 
-				// If DISCARD, we need to make sure that no current restriction 
-				// and nextRestriction need a parameter, to deal with RestrictionLogic.
-				if (restriction.getNullable().equals(Nullable.DISCARD)) {
-					if (isRestrictionParameterized && isNextRestrictionNeedParam) 
-						whereQueryString.
-							append(" ").
-							append(restriction.getRestrictionLogic().toLowerCase()).
-							append(" ");
-				} else if (restriction.getNullable().equals(Nullable.KEEP)) {
-					whereQueryString.
-						append(" ").
-						append(restriction.getRestrictionLogic().toLowerCase()).
-						append(" ");
+			// If DISCARD, we need to make sure that no current restriction 
+			// and nextRestriction need a parameter, to deal with RestrictionLogic.
+			final boolean nullableIsKeep = restriction.getNullable().equals(Nullable.KEEP);
+			final boolean nullableIsDiscard = nullableIsKeep == false;
+			final boolean isLastRestrictions = (i + 1) == restrictionSize;
+
+			if (nullableIsKeep && !isLastRestrictions) {
+				whereQueryString.append(" ").append(stringLogic).append(" ");
+			}
+
+			// If nullable is DISCARD, we need to make sure that restriction is also 
+			// parameterized. This is needed to avoid double logic added in 
+			// query string.
+			if (nullableIsDiscard && isRestrictionParameterized) {
+				if (!isLastRestrictions) {
+					whereQueryString.append(" ").append(stringLogic).append(" ");
 				}
-
-				logicCounter ++;
 			}
 
 		} // end of for.
 
 		if (whereQueryString.length() > 0) {
-			super.getQueryStringBuilder().append(" where ").append(whereQueryString.toString());
-			super.getCountQueryStringBuilder().append(" where ").append(whereQueryString.toString());
+			super.getQueryStringBuilder().
+				append(" where ").
+				append(this.getLastRestrictionString(whereQueryString));
+			
+			super.getCountQueryStringBuilder().
+				append(" where ").
+				append(this.getLastRestrictionString(whereQueryString));
 		}
 		return this;
 	}
@@ -165,9 +165,18 @@ public class JpaQueryBuilder extends AbstractQueryBuilder {
 		return result;
 	}
 
+
 	@Override
 	public String getEntityAliasName() {
 		return this.entityAliasName;
+	}
+
+	/**
+	 * Get the entity name.
+	 * @return {@link String} entity name.
+	 */
+	protected String getEntityName() {
+		return this.entityName;
 	}
 
 	/**
@@ -186,11 +195,27 @@ public class JpaQueryBuilder extends AbstractQueryBuilder {
 		}
 	}
 
-	protected String getEntityName() {
-		return this.entityName;
+	/**
+	 * Used to parse 'and' or 'or' string in the last of restriction query string.
+	 * @param whereRestriction the whole {@link Restriction} query string. Shouldn't 
+	 * be null.
+	 * @return parsed {@link Restriction} as query {@link String}.
+	 */
+	protected String getLastRestrictionString(StringBuilder whereRestriction) {
+		final int length = whereRestriction.toString().trim().length();
+		final int startForAnd = length - 3;
+		final int startForOr = length - 2;
+
+		if (whereRestriction.subSequence(startForAnd, length).toString().trim().equals("and"))
+			return whereRestriction.delete(startForAnd, length).toString().trim();
+
+		if (whereRestriction.subSequence(startForOr, length).toString().trim().equals("or")) 
+			return whereRestriction.delete(startForOr, length).toString().trim();
+
+		return whereRestriction.toString().trim();
 	}
 
-	private void createEntityAliasAndNameFromClass(Class<?> entityClass) {
+	private void createEntityAndAliasNameFromClass(Class<?> entityClass) {
 		if (entityClass.isAnnotationPresent(Entity.class) && 
 			!entityClass.getAnnotation(Entity.class).name().equals("")) 
 			this.entityName = entityClass.getAnnotation(Entity.class).name();
